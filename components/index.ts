84aac4ac9125705a96bd90d38970942ac5724967
何琦 <>
2023-01-07 23:11:25 +0800

    4.6 整理component目录

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,5 @@
-export { default as Navbar } from './navbar';
-export { default as BaseLayout } from './layout/BaseLayout';
-export { default as NftItem } from "./nft/item";
-export { default as NftList } from "./nft/list";
-export { default as ActiveLink } from "./link";
+export { default as Navbar } from './ui/navbar';
+export { default as BaseLayout } from './ui/layout/BaseLayout';
+export { default as NftItem } from "./ui/nft/item";
+export { default as NftList } from "./ui/nft/list";
+export { default as ActiveLink } from "./ui/link";

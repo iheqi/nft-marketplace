72eb825420db9d82f61ed6be2bf8f7b5820de11c
何琦 <>
2023-01-08 19:01:17 +0800

    5.1 Web3Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import { Web3Provider } from '../components/providers'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
     </>
   )
 }

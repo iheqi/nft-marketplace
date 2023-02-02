b3e4ca4ce776bc18fe435bf1eb8d93cfb107862e
qihe <>
2023-02-02 11:44:53 +0800

    7.2 hook 的 type

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,16 +1,17 @@
 // https://blog.csdn.net/lunahaijiao/article/details/108722424 没太懂这个库
+import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
 
 // deps -> provider, ethereum, contract (web3State)
-export const hookFactory = (deps: any) => (params: any) => {
+export const hookFactory: CryptoHookFactory = (deps) => (params) => {
   const swrRes = useSWR("web3/useAccount", () => {
     console.log(deps);
     console.log(params);
     // making request to get data
-    return "Test User"
+    return params;
   })
 
   return swrRes;
 }
 
-export const useAccount = hookFactory({ethereum: null, provider: null});
+export const useAccount = hookFactory({ethereum: undefined, provider: undefined});

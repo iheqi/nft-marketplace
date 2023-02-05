c074ca50ecc2e252a9a3ebc7247222b49d9a8a15
何琦 <>
2023-02-05 22:18:12 +0800

    10.8 合约 _createNftItem 函数

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -9,11 +9,26 @@ contract NftMarket is ERC721URIStorage {
   Counters.Counter private _listedItems; // 上架的nft数量
   Counters.Counter private _tokenIds;
 
-  mapping(string => bool) private _usedTokenURIs;
+  mapping(string => bool) private _usedTokenURIs; // 已有的tokenURI
+  mapping(uint => NftItem) private _idToNftItem;
+
+  struct NftItem {
+    uint tokenId;
+    uint price;
+    address creator;
+    bool isListed;
+  }
+
+  event NftItemCreated (
+    uint tokenId,
+    uint price,
+    address creator,
+    bool isListed
+  );
 
   constructor() ERC721("CreaturesNFT", "CNFT") {}
 
-  function mintToken(string memory tokenURI) public payable returns (uint) {
+  function mintToken(string memory tokenURI, uint price) public payable returns (uint) {
     require(!tokenURIExists(tokenURI), "Token URI already exists");
 
     _tokenIds.increment();
@@ -23,10 +38,29 @@ contract NftMarket is ERC721URIStorage {
 
     _safeMint(msg.sender, newTokenId);
     _setTokenURI(newTokenId, tokenURI);
+    _createNftItem(newTokenId, price);
     _usedTokenURIs[tokenURI] = true;
     
     return newTokenId;
   }
+
+
+  function _createNftItem(
+    uint tokenId,
+    uint price
+  ) private {
+    require(price > 0, "Price must be at least 1 wei");
+
+    _idToNftItem[tokenId] = NftItem(
+      tokenId,
+      price,
+      msg.sender,
+      true
+    );
+
+    emit NftItemCreated(tokenId, price, msg.sender, true);
+  }
+
 
   function tokenURIExists(string memory tokenURI) public view returns (bool) {
     return _usedTokenURIs[tokenURI] == true;

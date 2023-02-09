a66bb760ef1c2f873d0dd0117e8a6acdbf69b632
qihe <>
2023-02-09 21:26:35 +0800

    23.1 Upload Metadata to Pinata

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -1,8 +1,10 @@
+import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { Session } from "next-iron-session";
 import { NextApiRequest, NextApiResponse } from "next";
 import { withSession, contractAddress, addressCheckMiddleware } from "./utils";
 import { NftMeta } from "@_types/nft";
+import pinataConfig from "../../../pinata.config.js";
 
 export default withSession(async (req: NextApiRequest & {session: Session}, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -15,10 +17,22 @@ export default withSession(async (req: NextApiRequest & {session: Session}, res:
         return;
       }
 
-      const message = await addressCheckMiddleware(req, res);
+      await addressCheckMiddleware(req, res);
 
-      console.log('message', message);
-      res.status(200).send({message: "Nft has been created"});
+      // https://docs.pinata.cloud/pinata-api/pinning/pin-json
+      const jsonRes = await axios.post("https://api.pinata.cloud/pinning/pinJSONToIPFS", {
+        pinataMetadata: {
+          name: uuidv4()
+        },
+        pinataContent: nft
+      }, {
+        headers: {
+          pinata_api_key: pinataConfig.api_key,
+          pinata_secret_api_key: pinataConfig.api_secret
+        }
+      });
+      console.log('jsonRes', jsonRes);
+      return res.status(200).send(jsonRes.data);
     } catch {
       res.status(422).send({message: "Cannot create JSON"});
     } 

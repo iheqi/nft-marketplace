fa3c2ab1d64ea646475428f362459a3a36fea26d
qihe <>
2023-02-10 11:55:24 +0800

    23.6 上传图片到 pinata

--- pages/api/verify-image.ts
+++ pages/api/verify-image.ts
@@ -1,7 +1,11 @@
+import axios from "axios";
+import FormData from "form-data";
+import { v4 as uuidv4 } from "uuid";
 import { FileReq } from "@_types/nft";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Session } from "next-iron-session";
 import { addressCheckMiddleware, withSession } from "./utils";
+import pinataConfig from "../../../pinata.config.js";
 
 export default withSession(async (
   req: NextApiRequest & {session: Session}, 
@@ -19,13 +23,32 @@ export default withSession(async (
     }
 
     await addressCheckMiddleware(req, res);
+    const buffer = Buffer.from(Object.values(bytes));
+    const formData = new FormData();
 
-    console.log(fileName);
-    console.log(contentType);
-    console.log(bytes);
+    formData.append(
+      "file",
+      buffer, {
+        contentType,
+        filename: fileName + "-" + uuidv4()
+      }
+    );
+    
+    // https://docs.pinata.cloud/pinata-api/pinning/pin-file-or-directory
+    // 1.同一张图片其他人上传，得到的 IpfsHash 是一样的
+    // 2.同一个人上传同一张图片，会返回 isDuplicate: true 
+    
+    const fileRes = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+      maxBodyLength: Infinity,
+      headers: {
+        // boundary: https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+        "Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}`,
+        pinata_api_key: pinataConfig.api_key,
+        pinata_secret_api_key: pinataConfig.api_secret
+      }
+    });
 
-    return res.status(200).send({message: "Image has been created!"});
-
+    return res.status(200).send(fileRes.data);
   } else {
     return res.status(422).send({message: "Invalid endpoint"});
   }

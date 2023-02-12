7d11f87125f1a678a5f70165600497318688150f
何琦 <>
2023-02-12 18:33:07 +0800

    26.1 部署准备：ALCHEMY_GOERLI_URL & 配置

--- pages/api/utils.ts
+++ pages/api/utils.ts
@@ -26,10 +26,13 @@ export function withSession(handler: any) {
   })
 }
 
+const NETWORK_URL = process.env.NODE_ENV === 'production' ?
+  process.env.ALCHEMY_GOERLI_URL : "http://127.0.0.1:7545";
+
 export const addressCheckMiddleware = async (req: NextApiRequest & { session: Session}, res: NextApiResponse) => {
   return new Promise(async (resolve, reject) => {
     const message = req.session.get("message-session");
-    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
+    const provider = new ethers.providers.JsonRpcProvider(NETWORK_URL);
 
     const contract = new ethers.Contract(
       contractAddress,

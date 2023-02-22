069c41ba8489e861207dbb832ff4db58adad17ca
何琦 <>
2023-02-22 18:31:11 +0800

    feat: 接入TheGraph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,34 @@ import { Web3Provider } from '../components/providers'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "https://api.thegraph.com/subgraphs/name/iheqi/nft-marketplace",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        approvals(first: 5) {
+          id
+          owner
+          approved
+          tokenId
+        }
+        approvalForAlls(first: 5) {
+          id
+          owner
+          operator
+          approved
+        }
+      }
+    `,
+  })
+  .then((result) => console.log("result:", result));
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
